02eb7d35816f48275f2b516f1a096ecca18bb0f2
Moises088 <costamoises300@gmail.com>
2023-01-14 12:02:38 -0300

    feat: created card technology

--- src/app/modules/site/components/card-technology/card-technology.component.ts
+++ src/app/modules/site/components/card-technology/card-technology.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-technology',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CardTechnologyComponent {
 
+  @Input() title: string = '';
+  @Input() img: string = '';
 }
--- src/app/modules/site/pages/about-project/about-project.component.ts
+++ src/app/modules/site/pages/about-project/about-project.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AboutProjectComponent {
 
+  logos: Array<{ img: string, title: string }> = [
+    { img: './assets/logos/angular-mini.png', title: 'Angularjs' },
+    { img: './assets/logos/nestjs-mini.svg', title: 'Nestjs' },
+    { img: './assets/logos/react-mini.png', title: 'React Native' },
+    { img: './assets/logos/sql.png', title: 'MySQL' },
+    { img: './assets/logos/expo.png', title: 'Expo' },
+    { img: './assets/logos/github.png', title: 'Github' },
+    { img: './assets/logos/ts.png', title: 'Typescript' },
+  ]
 }
